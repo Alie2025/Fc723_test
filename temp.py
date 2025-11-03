cc838343d1d0ce6c0e6128cb5bd26a5c23332783
ali alsharshani <Ali>
2025-11-03 11:20:44 +0000

    first commit

---
+++ temp.py
@@ -0,0 +1,23 @@
+print("hello,world")
+# Hello,the world is ending.
+# Hello,Alice!
+print("what is going on")
+my_list=[ "apples" , "mango" , "tomatoes" and "kiwi" ]
+print(my_list)
+movies=[ "harry potter" , "the dark" , "stranger things" ]
+print(movies[2])
+person=( "Bob" , 24 , "happy" )
+print(person)
+#person[2]="computer scientisit"
+Animals={ "cats" , "dog" , "bird" }
+Animals.add("fish")
+print(Animals)
+Grades={ "Alice"}
+my_tuple = ( 1 , 2 , 17 , 23 )
+print(my_tuple [1])
+subjects = [ "maths" , "engineering" , "physics" , "computer science" ]
+
+i=1
+while i <= 20:
+    print(i)
+    i += 1
